2b2b412b68965d70ebd12a7a7c777e002a98d6ac
rpaadm <de.ronaldo@gmail.com>
2013-08-26 12:13:44 +0200

    google analytics | another test

--- client/portfolio.js
+++ client/portfolio.js
@@ -17,21 +17,19 @@ function collectionExtract(collection) {
 
 // setup
 Template.setup.rendered = function() {
-  if (!(window._gaq != null)) {
-    window._gaq = [];
-    _gaq.push(['_setAccount', 'UA-40144979-2']);
-    _gaq.push(['_trackPageview']);
-    return (function() {
-      var ga, gajs, s;
-      ga = document.createElement('script');
-      ga.type = 'text/javascript';
-      ga.async = true;
-      gajs = '.google-analytics.com/ga.js';
-      ga.src = 'https:' === document.location.protocol ? 'https://ssl' + gajs : 'http://www' + gajs;
-      s = document.getElementsByTagName('script')[0];
-      return s.parentNode.insertBefore(ga, s);
-    })();
-  }
+  (function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r;
+      i[r] = i[r] || function () {
+          (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date();
+      a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+  ga('create', 'UA-40144979-2', 'ronaldpauffert.fr');
+  ga('send', 'pageview');
 };
 
 // coverPage
