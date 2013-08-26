021c3c343184b1b51bb77fdaa2b8d2dfcc1e6e6f
rpaadm <de.ronaldo@gmail.com>
2013-08-26 11:09:17 +0200

    google analytics | test2

--- client/portfolio.js
+++ client/portfolio.js
@@ -15,6 +15,23 @@ function collectionExtract(collection) {
   return result;  
 }  
 
+// setup
+Template.setup.rendered = function() {
+  (function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r;
+      i[r] = i[r] || function () {
+          (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date();
+      a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+  ga('create', 'UA-40144979-2', 'ronaldpauffert.fr');
+  ga('send', 'pageview');
+};
+
 // coverPage
 Template.coverPage.container = function () {
   var cover = [];    
