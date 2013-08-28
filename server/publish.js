50e168ad74eeb1189e67356a72311ea357840ee0
rpaadm <de.ronaldo@gmail.com>
2013-08-29 00:57:37 +0200

    DRY - with pretty url

--- server/publish.js
+++ server/publish.js
@@ -13,21 +13,21 @@ Meteor.startup(function () {
   Pictures.remove({});
   Albums.remove({});  
 
-  function recordPictures(albumDirectory) {
+  function recordPictures(albumDirectory,albumUrl) {
     var pictures = fs.readdirSync(imagesDirectory + "/" + albumDirectory);
     pictures.forEach(function(picture, index) {
       Pictures.insert({ 
         fileName: picture, 
         path: albumDirectory,
-        url: prettyUrl.generate(albumDirectory),  
+        url: albumUrl,  
         cover: picture.match(beginByIdentifier) || {},
         index: index 
       });
     });         
   }
   for (var i = 0; i < albumDirectory.length; i++) {
-    recordPictures(albumDirectory[i]);
     albumUrl = prettyUrl.generate(albumDirectory[i]);
+    recordPictures(albumDirectory[i],albumUrl);
     // build album list for the menu
     Albums.insert({ 
       title: albumDirectory[i], 
